dc298e8f774d2643d452312534ae6caf8e339f2c
dymazoo <github@dymazoo.com>
2022-12-10 13:30:47 +0000

    Beta 4

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    apiHost: 'https://api.dymazoo.com/api/',
-    stripeKey: 'pk_test_51Lr1tyJcqMwFQOnWwbYB4ChPf5Lt5hqa7UDrN3lKMLlmVe4h7zJ8xWqzC6qQ2EYsAaYLcg6xyoC7DP8lAgVdAKX500MXd5JjyQ'
-};
